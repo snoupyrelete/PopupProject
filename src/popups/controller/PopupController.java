61e88c375dc17fd71ce8933773c552aeccacfed8
snoupyrelete <drob8896@740s-pl-112.local>
2016-10-04 09:46:32 -0600

    power blackout

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -82,5 +82,60 @@ public class PopupController
 		thingyList.add(testThingy);
 		display.displayMessage(thingyList.size() + " is the size of the list.");
 		testThingy.setValue(12.1);
+		for (int index = 0; index <5; index ++)
+		{
+			Thingy addThingy = new Thingy();
+			thingyList.add(addThingy);
+			display.displayMessage(addThingy.getWords());
+		
+		}
+		display.displayMessage(thingyList.size() + " is the size.");
+		thingyList.get(3).setWords("LOOK I CHANGED A VALUE.");
+		
+		display.displayMessage(thingyList.get(3) + "is @ 3");
+		
+//		Thingy temp = thingyList.get(4);
+//		temp.setWords("#SWAG");
+//		
+//		for (int index = 0; index < thingyList.size(); index ++)
+//		{
+//			display.displayMessage(thingyList.get(index).getWords());
+//		}
+//		
+//		thingyList.add(3, new Thingy());
+//		
+//		display.displayMessage("The size is " + thingyList.size());
+//		
+//		thingyList.remove(0); //I no longer have access to this object because there is no = sign.
+//		
+//		display.displayMessage("The size is NOW! " + thingyList.size());
+//		
+//		Thingy deletedThingy = thingyList.remove(3); // BECAUSE .REMOVE RETURNS A VALUE SO WE CAN USE IT!!!
+//		display.displayMessage(deletedThingy.getWords() + " and the list size is " + thingyList.size());
+//		
+//		thingyList.set(0, deletedThingy);
+//		
+//		for (int index = 0; index < thingyList.size(); index ++)
+//		{
+//			display.displayMessage(thingyList.get(index).getWords());
+//		}
+//		
+//		for (Thingy currentThingy : thingyList)
+//		{
+//			display.displayMessage(currentThingy.getWords());
+//		}
+		
+		String indexToChange = "ABC";
+		String whatToChangeTo = "XYZ";
+		while (indexToChange != null && !isInteger(indexToChange) && whatToChangeTo != null)
+		{
+			indexToChange = display.collectResponse("At what index should we change?.");
+			whatToChangeTo = display.collectResponse("What should we change it to?.");
+			int updatedIndex = Integer.parseInt(indexToChange);
+			thingyList.get(updatedIndex).setWords(whatToChangeTo);
+			display.displayMessage("You changed " + updatedIndex + "to " + whatToChangeTo);
+		}
+	
+		
 	}
 }
